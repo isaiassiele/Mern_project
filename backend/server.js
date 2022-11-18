b24c00675aa387e18d867d6d25bbfa32a0c4e452
Abhishek Kumar <abhishek.it.msit@gmail.com>
2022-11-18 13:23:29 +0000

    Mongo DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
 const app = require("./app");
 
 const dotenv = require("dotenv");
+const connectDatabase = require("./config/database")
 
 // Config
 
 dotenv.config({path:"backend/config/config.env"});
 
     
+// Connecting to databse
+
+connectDatabase()
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server is wokring on http://localhost:${process.env.PORT}`)
